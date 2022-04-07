710e372e457c4bfa251f8747e59c6480524da845
tcmle <tao.cao@outlook.com>
2022-04-07 14:12:48 +0100

    Update test.sh

--- lab8/tasks/test.sh
+++ lab8/tasks/test.sh
@@ -5,7 +5,7 @@ set +e
 FAILURE=false
 
 ./training/tests/test_run_experiment.sh || FAILURE=true
-pytest -v -rs --html=report.html --self-contained-html test_Suite.py -s . || FAILURE=true
+pytest -v -rs . --html=report.html --self-contained-html test_Suite.py || FAILURE=true
 
 if [ "$FAILURE" = true ]; then
   echo "Tests failed"
